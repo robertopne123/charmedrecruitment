5a6a9e2d8309c7c5b5a3937790b18b52a8dbe367
robertopne123 <robg.pne98@googlemail.com>
2022-03-14 12:47:50 +0000

    fixed navbar, added font

--- Components/jobcard.js
+++ Components/jobcard.js
@@ -7,11 +7,17 @@ export const JobCard = (props) => {
     // <Link>
     //   <a>
     <div className=" mx-auto p-[30px] flex flex-col relative w-[400px] h-[400px] drop-shadow-lg  ">
-      <div className="relative h-full w-[340px] ">
-        <Image src={props.image} alt="" layout="fill" objectFit="cover" />
+      <div className="relative h-full w-[340px] bg-primary">
+        <Image
+          src={props.image}
+          alt=""
+          layout="fill"
+          objectFit="cover"
+          className="opacity-30"
+        />
       </div>
-      <div className="absolute w-[100%] h-[100%] flex flex-col justify-center">
-        <h3 className=" text-white text-center font-semibold text-2xl mx-auto mt-[150px]">
+      <div className="absolute left-0 top-0 w-[100%] h-[100%] flex flex-col justify-center">
+        <h3 className=" text-white text-center font-semibold text-3xl mx-auto font-parkson">
           {props.name}
         </h3>
       </div>
