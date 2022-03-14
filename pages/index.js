a08a81c042b72593f390db40749857443835f507
Richard Van Parys <93936892+RegVP@users.noreply.github.com>
2022-03-14 11:17:31 +0000

    header updates

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Footer } from "../Components/Footer";
+import  Footer  from "../Components/Footer";
 import { Header } from "../components/header";
 import { JobCard } from "../components/jobcard";
 // import styles from "../styles/Home.module.css";
@@ -176,6 +176,8 @@ export default function Home() {
           </div>
         </div>
       </div>
+    <Footer />
     </div>
+    
   );
 }
